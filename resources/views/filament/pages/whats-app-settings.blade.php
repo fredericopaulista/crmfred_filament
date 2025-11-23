d9bd9c510e4eaf0060e97def78c770dcdf96b9a0
fredericopaulista <fredericopaulista@gmail.com>
2025-11-22 22:34:02 -0300

    feat: Add delayed connection status check and UI message for WhatsApp QR code.

--- resources/views/filament/pages/whats-app-settings.blade.php
+++ resources/views/filament/pages/whats-app-settings.blade.php
@@ -22,13 +22,24 @@
                 Escaneie o QR Code
             </x-slot>
 
-            <div class="flex flex-col items-center justify-center p-4">
+            <div 
+                class="flex flex-col items-center justify-center p-4"
+                x-data
+                x-init="
+                    if (@js($qr_code)) {
+                        setTimeout(() => {
+                            $wire.checkConnectionStatus();
+                        }, 15000);
+                    }
+                "
+            >
                 @if($qr_code)
                     <img src="{{ $qr_code }}" alt="WhatsApp QR Code" class="max-w-full border-4 border-white shadow-lg rounded-lg" />
                     <p class="mt-4 text-sm text-center text-gray-500">
                         Abra o WhatsApp no seu celular <br>
                         <strong>Menu > Aparelhos conectados > Conectar um aparelho</strong>
                     </p>
+                    <p class="mt-2 text-xs text-gray-400">Verificando conexão em 15 segundos...</p>
                 @else
                     <div class="flex items-center justify-center h-48 w-full bg-gray-100 rounded-lg">
                         <x-filament::loading-indicator class="h-10 w-10" />
